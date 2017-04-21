7e0f3f9bb8fcf799bcb7ea1446848ad918766e31
laporte11 <stefano.fatta@gmail.com>
2017-04-21 18:36:43 -0300

    Crossover PMX

--- gen-alg-8queens-advanced.py
+++ gen-alg-8queens-advanced.py
@@ -1,4 +1,4 @@
- import random
+import random
 import itertools
 
 
@@ -8,33 +8,41 @@ class Cromossomo:
         self.genes = genes
         self.fitness = fitness(genes)
 
-def crossover(pai1, pai2):
-    corte = int(random.uniform(1,6))
-    filho1 = []
-    filho2 = []
-#    print (corte+1)
-    for i in range(corte):
-        filho1.append(pai1[i])
-        filho2.append(pai2[i])
-        
-        
-    for i in range(corte, len(pai2)):
-        if(pai2[i] not in filho1):
-            filho1.append(pai2[i])
-            
-    for i in range(corte):
-        if(pai2[i] not in filho1):
-            filho1.append(pai2[i])
-    
-    for i in range(corte, len(pai1)):
-        if(pai1[i] not in filho2):
-            filho2.append(pai1[i])
-            
-    for i in range(corte):
-        if(pai1[i] not in filho2):
-            filho2.append(pai1[i])
+def crossover(ind1, ind2):
 
-    return filho1,filho2
+    size = min(len(ind1), len(ind2))
+    p1, p2 = [0]*size, [0]*size
+
+    ind1 = binVectorToIntVector(ind1)
+    ind2 = binVectorToIntVector(ind2)    
+    
+    for i in xrange(size):
+        p1[ind1[i]] = i
+        p2[ind2[i]] = i
+    
+    cxpoint1 = random.randint(0, size)
+    cxpoint2 = random.randint(0, size - 1)
+    if cxpoint2 >= cxpoint1:
+        cxpoint2 += 1
+    else: 
+        cxpoint1, cxpoint2 = cxpoint2, cxpoint1
+    
+
+    for i in xrange(cxpoint1, cxpoint2):
+
+        temp1 = ind1[i]
+        temp2 = ind2[i]
+
+        ind1[i], ind1[p1[temp2]] = temp2, temp1
+        ind2[i], ind2[p2[temp1]] = temp1, temp2
+        
+        p1[temp1], p1[temp2] = p1[temp2], p1[temp1]
+        p2[temp1], p2[temp2] = p2[temp2], p2[temp1]
+    
+    ind1 = intVectorToBinVector(ind1)
+    ind2 = intVectorToBinVector(ind2)      
+    
+    return ind1, ind2
 
     
 def mutacao(gene):
@@ -130,7 +138,7 @@ def main():
            
         #para mutacao seria
         #     random.uniform(0,1) <= 0.4
-        if(random.uniform(0,1) <= 0.4):
+        if(random.uniform(0,1) <= 0.1):
             if(genes1 != [] and genes2 != []):
                 genes1 = mutacao(genes1)
                 genes2 = mutacao(genes2)
